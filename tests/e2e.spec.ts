30ba47e8c34aaeba4c9e5efb501192e96322cc48
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2024-08-06 23:50:59 -0400

    fix: got e2e up and running

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,4 +1,3 @@
-import { createHash } from 'crypto';
 import type { BrowserWindow } from 'electron';
 import type { ElectronApplication, JSHandle } from 'playwright';
 import { _electron as electron } from 'playwright';
@@ -15,22 +14,19 @@ afterAll(async () => {
 });
 
 test('Main window state', async () => {
-  console.log('start');
   const page = await electronApp.firstWindow();
-  console.log('await electronApp.firstWindow');
+
   const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
-  console.log('await electronApp.browserWindow(page)');
+
   const windowState = await window.evaluate(
-    (
-      mainWindow,
-    ): Promise<{ isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean }> => {
+    (mainWindow): Promise<{ isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean }> => {
       const getState = () => ({
         isVisible: mainWindow.isVisible(),
         isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
         isCrashed: mainWindow.webContents.isCrashed(),
       });
 
-      return new Promise(resolve => {
+      return new Promise((resolve) => {
         /**
          * The main window is created hidden, and is shown only when it is ready.
          * See {@link ../packages/main/src/mainWindow.ts} function
@@ -42,8 +38,6 @@ test('Main window state', async () => {
     },
   );
 
-  console.log('await window.evaluate');
-
   expect(windowState.isCrashed, 'The app has crashed').toBeFalsy();
   expect(windowState.isVisible, 'The main window was not visible').toBeTruthy();
   expect(windowState.isDevToolsOpened, 'The DevTools panel was open').toBeFalsy();
@@ -51,51 +45,17 @@ test('Main window state', async () => {
 
 test('Main window web content', async () => {
   const page = await electronApp.firstWindow();
-  const element = await page.$('#root', { strict: true });
-  expect(element, 'Was unable to find the root element').toBeDefined();
-  expect((await element.innerHTML()).trim(), 'Window content was empty').not.equal('');
+  const root = await page.$('#root', { strict: true });
+
+  expect(root, 'Was unable to find the root element').toBeDefined();
+
+  const innerHTML = (await root?.innerHTML()) || '';
+  expect(innerHTML.trim(), 'Window content was empty').not.equal('');
 });
 
-test('Preload versions', async () => {
+test('Shows loading screen on startup', async () => {
   const page = await electronApp.firstWindow();
-  const versionsElement = page.locator('#process-versions');
-  expect(await versionsElement.count(), 'expect find one element #process-versions').toStrictEqual(
-    1,
-  );
+  const loadingScreen = await page.$('#loadingScreenContainer', { strict: true });
 
-  /**
-   * In this test we check only text value and don't care about formatting. That's why here we remove any space symbols
-   */
-  const renderedVersions = (await versionsElement.innerText()).replace(/\s/g, '');
-  const expectedVersions = await electronApp.evaluate(() => process.versions);
-
-  for (const expectedVersionsKey in expectedVersions) {
-    expect(renderedVersions).include(
-      `${expectedVersionsKey}:v${expectedVersions[expectedVersionsKey]}`,
-    );
-  }
-});
-
-test('Preload nodeCrypto', async () => {
-  const page = await electronApp.firstWindow();
-
-  // Test hashing a random string
-  const testString = Math.random().toString(36).slice(2, 7);
-
-  const rawInput = page.locator('input#reactive-hash-raw-value');
-  expect(
-    await rawInput.count(),
-    'expect find one element input#reactive-hash-raw-value',
-  ).toStrictEqual(1);
-
-  const hashedInput = page.locator('input#reactive-hash-hashed-value');
-  expect(
-    await hashedInput.count(),
-    'expect find one element input#reactive-hash-hashed-value',
-  ).toStrictEqual(1);
-
-  await rawInput.fill(testString);
-  const renderedHash = await hashedInput.inputValue();
-  const expectedHash = createHash('sha256').update(testString).digest('hex');
-  expect(renderedHash).toEqual(expectedHash);
+  expect(loadingScreen, 'Was unable to find the loading screen').toBeDefined();
 });
