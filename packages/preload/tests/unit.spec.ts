6bf066ce95e01f658085e6d57ddd2d006da07f3c
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2024-08-13 18:06:39 -0400

    chore: test spec file removed from preload pkg

--- packages/preload/tests/unit.spec.ts
+++
@@ -1,8 +0,0 @@
-import { vi } from 'vitest';
-
-// TODO: Remove this workaround after unplugin-auto-expose will be fixed for ESM support
-vi.mock('electron', () => ({
-  contextBridge: {
-    exposeInMainWorld: () => {},
-  },
-}));
