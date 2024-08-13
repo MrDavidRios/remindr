3982affaf506d824dd0df1fae0364aae26aadfe2
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2024-08-13 10:04:35 -0400

    chore: initial setup

--- vitest.config.js
+++ vitest.config.js
@@ -12,6 +12,10 @@ const config = {
      */
     include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
+    coverage: {
+      reporter: ['text', 'lcov'],
+    },
+
     /**
      * The default timeout of 5000ms is sometimes not enough for playwright.
      */
