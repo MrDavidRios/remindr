b258f76e3f889c4b34e92e5f870e474ac77673cb
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2024-08-13 15:27:03 -0400

    fix: getMainAssetPath test made os-agnostic

--- packages/main/tests/getMainAssetPath.spec.ts
+++ packages/main/tests/getMainAssetPath.spec.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { describe, expect, it, vi } from 'vitest';
 import { getMainAssetPath } from '../src/utils/getMainAssetPath';
 
@@ -15,13 +16,15 @@ describe('getMainAssetPath', () => {
     vi.stubEnv('PROD', true);
 
     const result = getMainAssetPath('test-file.txt');
-    expect(result).toBe('\\mocked\\app\\path\\packages\\main\\dist\\test-file.txt');
+    const expectedPath = path.join('/mocked/app/path', 'packages', 'main', 'dist', 'test-file.txt');
+    expect(result).toBe(expectedPath);
   });
 
   it('should return the correct path in development environment', () => {
     vi.stubEnv('PROD', false);
 
     const result = getMainAssetPath('test-file.txt');
-    expect(result).toBe('\\mocked\\app\\path\\packages\\main\\public\\test-file.txt');
+    const expectedPath = path.join('/mocked/app/path', 'packages', 'main', 'public', 'test-file.txt');
+    expect(result).toBe(expectedPath);
   });
 });
