fe99556fbef87267707f7c208d5f913963ace0a8
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2024-10-01 19:46:24 +0200

    feat: added task column behavior for completed tasks

--- packages/renderer/src/components/task-management-page/task-list-display/task-columns/TaskColumn.tsx
+++ packages/renderer/src/components/task-management-page/task-list-display/task-columns/TaskColumn.tsx
@@ -2,6 +2,7 @@ import plusIcon from '@assets/icons/plus.svg';
 import { Task } from '@remindr/shared';
 import { useAnimationsEnabled } from '@renderer/scripts/utils/hooks/useanimationsenabled';
 import { AnimatePresence, LayoutGroup, motion } from 'framer-motion';
+import React from 'react';
 import { AnimateChangeInHeight } from '../../AnimateChangeInHeight';
 import { TaskTileWrapper } from '../task-tile/TaskTileWrapper';
 
@@ -23,25 +24,46 @@ const TaskColumnActionBar: React.FC = () => {
   );
 };
 
+const InternalTaskList: React.FC<{ tasks: Task[] }> = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <div key={task.creationTime}>
+          <TaskTileWrapper task={task} reorderable={false} />
+        </div>
+      ))}
+    </>
+  );
+};
+
 export const TaskColumn: React.FC<TaskColumnProps> = ({ name, tasks }) => {
+  const animationsEnabled = useAnimationsEnabled();
+
+  const completeTasks = tasks.filter((task) => task.completed);
+  const incompleteTasks = tasks.filter((task) => !task.completed);
+
   return (
     <div className="task-column">
       <AnimateChangeInHeight show>
         <h2>{name}</h2>
         <div className="tasks">
-          {tasks.length === 0 ? (
-            <p className="no-tasks-message">All done here!</p>
-          ) : (
-            <LayoutGroup>
-              <AnimatePresence mode="popLayout">
-                {tasks.map((task) => (
-                  <div key={task.creationTime}>
-                    <TaskTileWrapper task={task} reorderable={false} />
-                  </div>
-                ))}
-              </AnimatePresence>
-            </LayoutGroup>
-          )}
+          <LayoutGroup>
+            <AnimatePresence mode="popLayout">
+              {incompleteTasks.length === 0 ? (
+                <p className="no-tasks-message">All done here!</p>
+              ) : (
+                <InternalTaskList tasks={incompleteTasks} />
+              )}
+              {completeTasks.length > 0 && (
+                <>
+                  <motion.p className="complete-tasks-header" layout={animationsEnabled ? 'position' : false}>
+                    Completed
+                  </motion.p>
+                  <InternalTaskList tasks={completeTasks} />
+                </>
+              )}
+            </AnimatePresence>
+          </LayoutGroup>
         </div>
         <TaskColumnActionBar />
       </AnimateChangeInHeight>
