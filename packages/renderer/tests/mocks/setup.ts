2a8463c61e4cb0a2afd012d4f34d7dd82be321d8
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2025-03-23 16:14:32 -0400

    chore: coverage reinstated

--- packages/renderer/tests/mocks/setup.ts
+++ packages/renderer/tests/mocks/setup.ts
@@ -1,29 +1,29 @@
-import { createDefaultSettings } from '@remindr/shared';
-import '@testing-library/jest-dom/vitest';
-import { configure } from '@testing-library/react';
-import { vi } from 'vitest';
+import { createDefaultSettings, Settings } from "@remindr/shared";
+import "@testing-library/jest-dom/vitest";
+import { configure } from "@testing-library/react";
+import { vi } from "vitest";
 
 // Configuration to disable pretty print DOM on error (https://stackoverflow.com/a/64155473/5750490)
 configure({
   getElementError: (message: string | null) => {
-    const error = new Error(message ?? 'Unknown error');
-    error.name = 'TestingLibraryElementError';
+    const error = new Error(message ?? "Unknown error");
+    error.name = "TestingLibraryElementError";
     error.stack = undefined;
     return error;
   },
 });
 
-vi.mock('@renderer/scripts/utils/userData', () => {
+vi.mock("@renderer/scripts/utils/userData", () => {
   return {};
 });
 
-vi.mock('@renderer/scripts/systems/authentication', () => {
+vi.mock("@renderer/scripts/systems/authentication", () => {
   return {
     getEmailVerifiedValue: vi.fn(() => true),
   };
 });
 
-vi.mock('@renderer/app/store', () => {
+vi.mock("@renderer/app/store", () => {
   return {
     default: {
       getState: vi.fn(),
@@ -31,14 +31,16 @@ vi.mock('@renderer/app/store', () => {
   };
 });
 
-vi.mock('@renderer/features/task-list/taskListSync', () => {
+vi.mock("@renderer/features/task-list/taskListSync", () => {
   return {
     initializeTaskListSyncListener: vi.fn(() => {}),
   };
 });
 
-vi.mock('@renderer/scripts/utils/taskfunctions', async () => {
-  const taskFunctions = await vi.importActual('@renderer/scripts/utils/taskfunctions');
+vi.mock("@renderer/scripts/utils/taskfunctions", async () => {
+  const taskFunctions = await vi.importActual(
+    "@renderer/scripts/utils/taskfunctions"
+  );
 
   return {
     ...taskFunctions,
@@ -46,37 +48,91 @@ vi.mock('@renderer/scripts/utils/taskfunctions', async () => {
   };
 });
 
-vi.mock('@renderer/scripts/utils/hooks/usedetectwheel', () => {
+vi.mock("@renderer/scripts/utils/hooks/usedetectwheel", () => {
   return {
     useDetectWheel: vi.fn(),
   };
 });
 
+// Mock window
 // https://stackoverflow.com/questions/57311971/error-not-implemented-window-scrollto-how-do-we-remove-this-error-from-jest-t
 window.scrollTo = vi.fn();
 
-(window as any).data = {
+declare global {
+  interface Window {
+    userState: {
+      setUserProfile: (profile: string) => void;
+      loadSettings: () => Settings;
+    };
+    data: {
+      saveTaskData: (data: string) => void;
+      saveUserData: (data: string) => void;
+    };
+    appState: {
+      syncAppMode: (mode: string) => void;
+    };
+    firebase: {
+      auth: {
+        authCredentialExists: () => void;
+        emailVerified: () => void;
+        getCurrentUser: () => string | null;
+      };
+    };
+    electron: {
+      ipcRenderer: {
+        on: (channel: string, func: (...args: unknown[]) => void) => void;
+        once: (channel: string, func: (...args: unknown[]) => void) => void;
+        sendMessage: (channel: string, ...args: unknown[]) => void;
+      };
+      remote: {
+        isPackaged: () => boolean;
+        isDebug: () => boolean;
+        openDevTools: () => void;
+      };
+      shell: {
+        openExternal: (url: string) => void;
+        showInFolder: (path: string) => void;
+        getPageTitle: (url: string) => string;
+      };
+      theme: {
+        getSystemTheme: () => string;
+      };
+      process: {
+        isWindowsOrMac: () => boolean;
+        isWindows: () => boolean;
+        isMac: () => boolean;
+        isLinux: () => boolean;
+      };
+      path: {
+        basename: (path: string) => string;
+      };
+    };
+  }
+}
+
+window.data = {
   saveTaskData: vi.fn(),
   saveUserData: vi.fn(),
 };
 
-(window as any).userState = {
+window.userState = {
   setUserProfile: vi.fn(),
   loadSettings: vi.fn(() => createDefaultSettings()),
 };
 
-(window as any).appState = {
+window.appState = {
   syncAppMode: vi.fn(() => {}),
 };
 
-(window as any).firebase = {
+window.firebase = {
   auth: {
     authCredentialExists: vi.fn(() => {}),
     emailVerified: vi.fn(() => {}),
+    getCurrentUser: vi.fn(() => null),
   },
 };
 
-(window as any).electron = {
+window.electron = {
   ipcRenderer: {
     on: vi.fn(() => vi.fn()),
     once: vi.fn(),
---
+++ packages/renderer/vitest.config.ts
@@ -0,0 +1,17 @@
+import { mergeConfig } from "vite";
+import { defineConfig } from "vitest/config";
+import viteConfig from "./vite.config";
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: "happy-dom",
+      globals: true,
+      setupFiles: ["./tests/mocks/setup.ts"],
+      coverage: {
+        reporter: ["text", "lcov"],
+      },
+    },
+  })
+);
